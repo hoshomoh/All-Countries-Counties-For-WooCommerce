c4348452af32164a68b403d7ccbe33975f3651e4
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-09-21 19:30:57 +0100

    Added gitignore file Fixed grammatical error with Abia state Local Government

--- local-governments/NG/AB.php
+++ local-governments/NG/AB.php
@@ -1,29 +1,29 @@
 <?php
 /**
- * Nigeria Abia State's Local Government
+ * Nigeria Abia State Local Government's
  *
  * @author   Hoshomoh
  * @version  1.0.2
  */
 
 $local_governments ['NG']['AB' ] =
-  [
-      '' => __( 'Select an option...' , 'woocommerce' ),
-      'BN' => __( 'Bende' , 'woocommerce' ),
-      'IK' => __( 'Isuikwuato' , 'woocommerce' ),
-      'UN' => __( 'Umu Nneochi' , 'woocommerce' ),
-      'IN' => __( 'Isiala-ngwa North' , 'woocommerce' ),
-      'IS' => __( 'Isiala-ngwa South' , 'woocommerce' ),
-      'ON' => __( 'Osisioma Ngwa' , 'woocommerce' ),
-      'AN' => __( 'Aba North' , 'woocommerce' ),
-      'AS' => __( 'Aba North' , 'woocommerce' ),
-      'OW' => __( 'Obi Ngwa' , 'woocommerce' ),
-      'UA' => __( 'Ugwunagbo' , 'woocommerce' ),
-      'UW' => __( 'Ukwa West' , 'woocommerce' ),
-      'UE' => __( 'Ukwa East' , 'woocommerce' ),
-      'OH' => __( 'Ohafia' , 'woocommerce' ),
-      'AC' => __( 'Arochukwu' , 'woocommerce' ),
-      'MN' => __( 'Umuahia North' , 'woocommerce' ),
-      'MS' => __( 'Umuahia South' , 'woocommerce' ),
-      'IK' => __( 'Ikwauano' , 'woocommerce' )
-  ];
+    [
+        '' => __( 'Select an option...' , 'woocommerce' ),
+        'BN' => __( 'Bende' , 'woocommerce' ),
+        'IK' => __( 'Isuikwuato' , 'woocommerce' ),
+        'UN' => __( 'Umu Nneochi' , 'woocommerce' ),
+        'IN' => __( 'Isiala-ngwa North' , 'woocommerce' ),
+        'IS' => __( 'Isiala-ngwa South' , 'woocommerce' ),
+        'ON' => __( 'Osisioma Ngwa' , 'woocommerce' ),
+        'AN' => __( 'Aba North' , 'woocommerce' ),
+        'AS' => __( 'Aba North' , 'woocommerce' ),
+        'OW' => __( 'Obi Ngwa' , 'woocommerce' ),
+        'UA' => __( 'Ugwunagbo' , 'woocommerce' ),
+        'UW' => __( 'Ukwa West' , 'woocommerce' ),
+        'UE' => __( 'Ukwa East' , 'woocommerce' ),
+        'OH' => __( 'Ohafia' , 'woocommerce' ),
+        'AC' => __( 'Arochukwu' , 'woocommerce' ),
+        'MN' => __( 'Umuahia North' , 'woocommerce' ),
+        'MS' => __( 'Umuahia South' , 'woocommerce' ),
+        'IK' => __( 'Ikwauano' , 'woocommerce' )
+    ];
