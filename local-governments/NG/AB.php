5960aa701f724467b2985ff62f92ce734d743713
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-09-21 18:57:38 +0100

    Added Full list of Nigeria Abia State's Local Government

--- local-governments/NG/AB.php
+++ local-governments/NG/AB.php
@@ -1,13 +1,29 @@
 <?php
 /**
- * Nigerian provinces
+ * Nigeria Abia State's Local Government
  *
  * @author   Hoshomoh
- * @version  1.0.0
+ * @version  1.0.2
  */
 
 $local_governments ['NG']['AB' ] =
   [
       '' => __( 'Select an option...' , 'woocommerce' ),
-      'TS' => __( 'Test Local Government' , 'woocommerce' )
+      'BN' => __( 'Bende' , 'woocommerce' ),
+      'IK' => __( 'Isuikwuato' , 'woocommerce' ),
+      'UN' => __( 'Umu Nneochi' , 'woocommerce' ),
+      'IN' => __( 'Isiala-ngwa North' , 'woocommerce' ),
+      'IS' => __( 'Isiala-ngwa South' , 'woocommerce' ),
+      'ON' => __( 'Osisioma Ngwa' , 'woocommerce' ),
+      'AN' => __( 'Aba North' , 'woocommerce' ),
+      'AS' => __( 'Aba North' , 'woocommerce' ),
+      'OW' => __( 'Obi Ngwa' , 'woocommerce' ),
+      'UA' => __( 'Ugwunagbo' , 'woocommerce' ),
+      'UW' => __( 'Ukwa West' , 'woocommerce' ),
+      'UE' => __( 'Ukwa East' , 'woocommerce' ),
+      'OH' => __( 'Ohafia' , 'woocommerce' ),
+      'AC' => __( 'Arochukwu' , 'woocommerce' ),
+      'MN' => __( 'Umuahia North' , 'woocommerce' ),
+      'MS' => __( 'Umuahia South' , 'woocommerce' ),
+      'IK' => __( 'Ikwauano' , 'woocommerce' )
   ];
