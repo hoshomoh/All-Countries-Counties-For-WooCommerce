bb787c96a7066081ef884d9617eed020e89d4722
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-09-24 19:34:08 +0100

    Added Nigerian LGA for Ekiti, Enugu, Gombe, Imo

--- local-governments/NG/EK.php
+++ local-governments/NG/EK.php
@@ -8,5 +8,21 @@
 
 $local_governments ['NG']['EK' ] =
     [
-        '' => __( 'Select an option...' , 'woocommerce' )
+        '' => __( 'Select an option...' , 'woocommerce' ),
+        'Ado Ekiti' => __( 'Ado Ekiti', 'woocommerce' ),
+        'Ikere' => __( 'Ikere', 'woocommerce' ),
+        'Irepodun' => __( 'Irepodun', 'woocommerce' ),
+        'Emure' => __( 'Emure', 'woocommerce' ),
+        'Ise Orun' => __( 'Ise Orun', 'woocommerce' ),
+        'Ekiti Sw.' => __( 'Ekiti Sw.', 'woocommerce' ),
+        'Ekiti West' => __( 'Ekiti West', 'woocommerce' ),
+        'Efon' => __( 'Efon', 'woocommerce' ),
+        'Ikole' => __( 'Ikole', 'woocommerce' ),
+        'Ekiti East' => __( 'Ekiti East', 'woocommerce' ),
+        'Gbonyin' => __( 'Gbonyin', 'woocommerce' ),
+        'Oye' => __( 'Oye', 'woocommerce' ),
+        'Ido-osi' => __( 'Ido-osi', 'woocommerce' ),
+        'Ijero' => __( 'Ijero', 'woocommerce' ),
+        'Moba' => __( 'Moba', 'woocommerce' ),
+        'Ilejemeje' => __( 'Ilejemeje', 'woocommerce' )
     ];
